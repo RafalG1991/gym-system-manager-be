b99034e364febdd927dac8b0c0a2e33309da30a0
Rafal <rgrajcar@gmail.com>
2022-06-13 22:12:28 +0200

    added cors, rateLimiter, cookieParser, .env and async errors initial config

--- index.ts
+++ index.ts
@@ -1,6 +1,24 @@
 import * as express from 'express';
+import * as cors from 'cors';
+import 'express-async-errors';
+import * as cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
+import 'dotenv/config';
+// import * as passport from 'passport';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 100,
+}));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Test' });
