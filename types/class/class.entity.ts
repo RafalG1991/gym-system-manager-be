06f826f5311f8d2f39b72bd345e62dc17612013d
Rafal <rgrajcar@gmail.com>
2022-07-05 20:35:17 +0200

    added class type

---
+++ types/class/class.entity.ts
@@ -0,0 +1,18 @@
+enum DayOfTheWeek {
+  SUNDAY = 'SUNDAY',
+  MONDAY = 'MONDAY',
+  TUESDAY = 'TUESDAY',
+  WEDNESDAY = 'WEDNESDAY',
+  THURSDAY = 'THURSDAY',
+  FRIDAY = 'FRIDAY',
+  SATURDAY = 'SATURDAY',
+}
+
+export interface ClassEntity {
+  id: string;
+  name: string;
+  description: string;
+  starts: string;
+  ends: string;
+  day: DayOfTheWeek;
+}
