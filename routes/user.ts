9f44ffdca9f409e5d72a9641bd2a9c5f2f9ccd5b
Rafal <rgrajcar@gmail.com>
2022-07-05 19:56:47 +0200

    added more informative error messages

--- routes/user.ts
+++ routes/user.ts
@@ -9,7 +9,7 @@ export const userRouter = Router();
 userRouter
   .post('/signup', async (req, res) => {
     const user = await UserRecord.getOneByEmail(req.body.email);
-    if (user) return res.status(200).json({ err: 'Email already in use' });
+    if (user) return res.status(400).json({ err: 'Email already in use!' });
     const newUser: UserRecord = new UserRecord(req.body);
     const id = await newUser.addOne();
     const { token, expires } = issueJWT(newUser);
@@ -22,9 +22,9 @@ userRouter
       .json({ sub: id, expires });
   })
   .post('/login', async (req, res) => {
-    if (!req.body.email || !req.body.password) return res.status(401).json({ err: 'Invalid data' });
+    if (!req.body.email || !req.body.password) return res.status(401).json({ err: 'Invalid login or password' });
     const user = await UserRecord.getOneByEmail(req.body.email);
-    if (!user) return res.status(401).json({ err: 'Invalid data' });
+    if (!user) return res.status(401).json({ err: 'Invalid login or password' });
     const isValid = await checkPassword(req.body.password, user.password);
     if (isValid) {
       const { token, expires } = issueJWT(user);
@@ -36,23 +36,38 @@ userRouter
         })
         .json({ sub: user.id, expires });
     }
-    return res.status(401).json({ err: 'Invalid data' });
+    return res
+      .status(401)
+      .json({ err: 'Invalid login or password' });
   })
   .get('/me', verifyUser, (req: UserAuthRequest, res) => {
-    res
-      .json(req.user);
+    try {
+      return res
+        .status(200)
+        .json(req.user);
+    } catch (e) {
+      return res
+        .status(401)
+        .json({ err: 'Error occurred during downloading data! Try again later!' });
+    }
   })
   .get('/data', verifyUser, async (req: UserAuthRequest, res) => {
-    const user = await UserRecord.getOneById(req.user.sub);
-    res.json({
-      email: user.email,
-      firstname: user.firstname,
-      lastname: user.lastname,
-      height: user.height,
-      weight: user.weight,
-      memberSince: user.memberSince,
-      membershipDate: user.membershipDate,
-    } as UserDataResponse);
+    try {
+      const user = await UserRecord.getOneById(req.user.sub);
+      return res.json({
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        height: user.height,
+        weight: user.weight,
+        memberSince: user.memberSince,
+        membershipDate: user.membershipDate,
+      } as UserDataResponse);
+    } catch (e) {
+      return res
+        .status(400)
+        .json({ err: 'Error occurred during downloading data! Try again later!' });
+    }
   })
   .patch('/', verifyUser, async (req: UserAuthRequest, res) => {
     const user = await UserRecord.getOneById(req.user.sub);
@@ -81,7 +96,7 @@ userRouter
     }
     return res
       .status(401)
-      .json({ err: 'Invalid data' });
+      .json({ err: 'Data cannot be changed! Try again later!' });
   })
   .patch('/membership', verifyUser, async (req: UserAuthRequest, res) => {
     try {
@@ -94,6 +109,6 @@ userRouter
       console.log(e);
       return res
         .status(401)
-        .json({ err: 'Invalid data' });
+        .json({ err: 'Membership cannot be extented! Try again later!' });
     }
   });
