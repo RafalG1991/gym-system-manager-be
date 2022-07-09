9df0f259b93d7da5de9727a69618e072d3b2aac4
Rafal <rgrajcar@gmail.com>
2022-07-09 21:43:18 +0200

    added remote cors origin

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ passport.use(strategy);
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
   credentials: true,
 }));
 
