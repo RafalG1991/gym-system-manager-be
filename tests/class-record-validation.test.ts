9acb6fab3513fa318f199685e28d47ef2ce20126
Rafal <rgrajcar@gmail.com>
2022-07-11 18:56:19 +0200

    added tests for class record and user record

--- tests/class-record-validation.test.ts
+++ tests/class-record-validation.test.ts
@@ -9,42 +9,92 @@ const defaultObj = {
   day: 5,
 };
 
-test('Name validation - not empty and maximum length is 50 characters', () => {
-  expect(() => new ClassRecord(defaultObj)).not.toThrow();
-  expect(() => new ClassRecord({
-    ...defaultObj,
-    name: '',
-  })).toThrow('Provide valid name with maximum length of 50 characters');
+describe('Name validation', () => {
+  test('Name validation - not empty', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      name: '',
+    })).toThrow('Provide valid name with maximum length of 50 characters');
+  });
+
+  test('Name validation - maximum length is 50 characters', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      name: 'fgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocasfdfsdiu3ibf',
+    })).toThrow('Provide valid name with maximum length of 50 characters');
+  });
 });
 
-test('Description validation - not be empty and maximum length is 400 characters', () => {
-  expect(() => new ClassRecord(defaultObj)).not.toThrow();
-  expect(() => new ClassRecord({
-    ...defaultObj,
-    description: '',
-  })).toThrow('Provide valid description with maximum length of 50 characters');
+describe('Description validation', () => {
+  test('Description validation - not be empty', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      description: '',
+    })).toThrow('Provide valid description with maximum length of 50 characters');
+  });
+
+  test('Description validation - maximum length is 400 characters', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      description: 'fgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocasfdfsdiu3ibffgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocasfdfsdiu3ibffgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocasfdfsdiu3ibffgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocasfdfsdiu3ibffgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocasfdfsdiu3ibffgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocasfdfsdiu3ibffgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocasfdfsdiu3ibffgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocasfdfsdiu3ibf',
+    })).toThrow('Provide valid description with maximum length of 50 characters');
+  });
 });
 
-test('Start time validation - not be empty and match the HH:MM time pattern - 24H with optional leading zero', () => {
-  expect(() => new ClassRecord(defaultObj)).not.toThrow();
-  expect(() => new ClassRecord({
-    ...defaultObj,
-    starts: '25:88',
-  })).toThrow('Provide valid start time');
+describe('Start time validation', () => {
+  test('Start time validation - not be empty', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      starts: '',
+    })).toThrow('Provide valid start time');
+  });
+
+  test('Start time validation - matches the HH:MM:SS time pattern - 24H with optional leading zero', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      starts: '25:88',
+    })).toThrow('Provide valid start time');
+  });
 });
 
-test('End time validation - not be empty and match the HH:MM time pattern - 24H with optional leading zero', () => {
-  expect(() => new ClassRecord(defaultObj)).not.toThrow();
-  expect(() => new ClassRecord({
-    ...defaultObj,
-    ends: '25:88',
-  })).toThrow('Provide valid end time');
+describe('End time validation', () => {
+  test('End time validation - not be empty', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      ends: '',
+    })).toThrow('Provide valid end time');
+  });
+
+  test('End time validation - matches the HH:MM:SS time pattern - 24H with optional leading zero', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      ends: '25:88',
+    })).toThrow('Provide valid end time');
+  });
 });
 
-test('Day of the week number validation - must be a number in range from 0 to 6 - not less than 0 and not greater than 6', () => {
-  expect(() => new ClassRecord(defaultObj)).not.toThrow();
-  expect(() => new ClassRecord({
-    ...defaultObj,
-    day: -1,
-  })).toThrow('Provide valid day of the week number');
+describe('Day of the week number validation', () => {
+  test('Day of the week number validation - must not be less than 0', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      day: -1,
+    })).toThrow('Provide valid day of the week number');
+  });
+
+  test('Day of the week number validation - must not be greater than 6', () => {
+    expect(() => new ClassRecord(defaultObj)).not.toThrow();
+    expect(() => new ClassRecord({
+      ...defaultObj,
+      day: 7,
+    })).toThrow('Provide valid day of the week number');
+  });
 });
--- tests/user-record-validation.test.ts
+++ tests/user-record-validation.test.ts
@@ -11,19 +11,139 @@ const defaultObj = {
   memberSince: '01-01-2022',
   membershipDate: '01-12-2022',
 };
+describe('Email validation', () => {
+  test('Email validation - not empty string', () => {
+    expect(() => new UserRecord(defaultObj)).not.toThrow();
+    expect(() => new UserRecord({
+      ...defaultObj,
+      email: '',
+    })).toThrow('Provide valid email');
+  });
 
-test('Email validation - not empty and maximum length is 320 characters and must have a valid email structure', () => {
-  expect(() => new UserRecord(defaultObj)).not.toThrow();
-  expect(() => new UserRecord({
-    ...defaultObj,
-    email: '',
-  })).toThrow('Provide valid email');
+  test('Email validation - maximum length is 320 characters', () => {
+    expect(() => new UserRecord(defaultObj)).not.toThrow();
+    expect(() => new UserRecord({
+      ...defaultObj,
+      email: 'fgdgnuiwefiuasbcncuizhc8awhoasnslcnsaocas8qrq0rqjfajfposmclznvkvnkvxuiovhhh48h9292859380hr0qwhf0ah0fazvznoafniaoufioaufoiuafiafousahfuioahfuioashfioasofaosifhashfahosfhashofaofhoho@fhfhfodfpdhvphv89xv8hv8dvopsdjvowjtp2ut0923ut032jtqijfiopajpoizjvzv-zu0vu-0u-3qfiojaopivjzpvzxuvuqru1u2ur-2u1021eoiqwjadsa-asufhiaoasodasofidajf.com',
+    })).toThrow('Provide valid email');
+  });
+
+  test('Email validation - must have a valid email structure', () => {
+    expect(() => new UserRecord(defaultObj)).not.toThrow();
+    expect(() => new UserRecord({
+      ...defaultObj,
+      email: 'notvalidemail',
+    })).toThrow('Provide valid email');
+  });
 });
 
-test('Password validation - not be empty and minimum length is 8 characters', () => {
-  expect(() => new UserRecord(defaultObj)).not.toThrow();
-  expect(() => new UserRecord({
-    ...defaultObj,
-    password: 'test',
-  })).toThrow('Password must be at least 8 characters');
+describe('Password validation', () => {
+  test('Password validation - not be empty string', () => {
+    expect(() => new UserRecord(defaultObj)).not.toThrow();
+    expect(() => new UserRecord({
+      ...defaultObj,
+      password: '',
+    })).toThrow('Password must be at least 8 characters');
+  });
+  test('Password validation - minimum length is 8 characters', () => {
+    expect(() => new UserRecord(defaultObj)).not.toThrow();
+    expect(() => new UserRecord({
+      ...defaultObj,
+      password: 'test',
+    })).toThrow('Password must be at least 8 characters');
+  });
+});
+
+describe('updateName method validation', () => {
+  test('updateName method throws an error if first name is an empty string', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      firstname: '',
+    });
+    await expect(testUser.updateName()).rejects.toThrow('Provide your first name and last name');
+  });
+
+  test('updateName method throws an error if first name contains white spaces only', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      firstname: '     ',
+    });
+    await expect(testUser.updateName()).rejects.toThrow('Provide your first name and last name');
+  });
+
+  test('updateName method throws an error if last name is an empty string', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      lastname: '',
+    });
+    await expect(testUser.updateName()).rejects.toThrow('Provide your first name and last name');
+  });
+
+  test('updateName method throws an error if last name contains white spaces only', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      lastname: '    ',
+    });
+    await expect(testUser.updateName()).rejects.toThrow('Provide your first name and last name');
+  });
+});
+
+describe('updateBmiData method validation', () => {
+  test('updateBmiData method throws an error if height is null', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      height: null,
+    });
+    await expect(testUser.updateBmiData()).rejects.toThrow('Provide valid height and weight');
+  });
+
+  test('updateBmiData method throws an error if parsed height is less than 0', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      height: '-10',
+    });
+    await expect(testUser.updateBmiData()).rejects.toThrow('Provide valid height and weight');
+  });
+
+  test('updateBmiData method throws an error if parsed height is greater than 300', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      height: '350',
+    });
+    await expect(testUser.updateBmiData()).rejects.toThrow('Provide valid height and weight');
+  });
+
+  test('updateBmiData method throws an error if weight is null', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      weight: null,
+    });
+    await expect(testUser.updateBmiData()).rejects.toThrow('Provide valid height and weight');
+  });
+
+  test('updateBmiData method throws an error if parsed weight is less than 0', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      weight: '-10',
+    });
+    await expect(testUser.updateBmiData()).rejects.toThrow('Provide valid height and weight');
+  });
+
+  test('updateBmiData method throws an error if parsed weight is greater than 999', async () => {
+    expect.assertions(1);
+    const testUser = new UserRecord({
+      ...defaultObj,
+      weight: '1000',
+    });
+    await expect(testUser.updateBmiData()).rejects.toThrow('Provide valid height and weight');
+  });
 });
