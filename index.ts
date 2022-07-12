100352784ffd0ddb83786f03be1eb5eff1a3d559
Rafal <rgrajcar@gmail.com>
2022-07-12 18:22:02 +0200

    removed cors_origin env

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ passport.use(strategy);
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
 
